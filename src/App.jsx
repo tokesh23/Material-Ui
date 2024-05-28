d3f3e2233889a7c3a92639aa55995907924efa4a
tokesh23 <tokesh1998@gmail.com>
2024-05-28 12:16:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+ import React from 'react'
+ 
+ import SearchBox from './SearchBox.jsx'
+ 
+ const App = () => {
+   return (
+     <div>
+      <SearchBox/>
+     </div>
+   )
+ }
+ 
+ export default App
---
+++ src/SearchBox.jsx
@@ -0,0 +1,14 @@
+ 
+ import React from 'react'
+ import TextField from '@mui/material/TextField';
+
+ const SearchBox = () => {
+   return (
+     <div><h3>Search for the weather</h3>
+     <form>
+     <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </form>
+        </div>
+   )
+ }
+ export default SearchBox
